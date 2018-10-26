0266f4ffb93f75d7a498c11ec27491effce4aedf
Will Spicher <guitarsenall@gmail.com>
2018-10-26 10:26:28 -0500

    Learning TortoiseGIT.

--- scratch.py
+++ scratch.py
@@ -1,7 +1,7 @@
 
 # scratch.py
 
-
+# image filename processing
 import os
 directory   = os.getcwd()
 extensions = ['jpg', 'bmp', 'png', 'gif']
@@ -9,8 +9,7 @@ image_file_names    = [ fn for fn in os.listdir(directory)    \
                         if any(fn.endswith(ext) for ext in extensions)]
 line    = '   see github_repository_a.png'
 print any( [line.find(fn) != -1 for fn in image_file_names] )
-
-for fn in file_names:
+for fn in image_file_names:
     if line.find(fn) != -1:
         fnlink  = '<A TARGET="_BLANK" HREF="%s">%s</A>' % (fn, fn)
         newline = line.replace( fn, fnlink )
